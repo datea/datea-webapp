6522565c7d829cc74ff59054ed8560360de16545
Juan La Jara <juanlajara@gmail.com>
2013-10-23 18:52:58 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module( 'dateaWebApp', [ ] )
+	.config( [ '$routeProvider', function ( $routeProvider ) {
+
+		$routeProvider
+		.when( '/' , { templateUrl : 'views/main.html'
+		             , controller  : 'MainCtrl'
+		             } )
+		.otherwise( { redirectTo : '/' } );
+
+	} ] );
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module( 'dateaWebApp' )
+	.controller( 'MainCtrl', [ '$scope', function ( $scope ) {
+
+		$scope.datea = {};
+		$scope.datea.name = 'Datea.pe';
+
+	} ] );
