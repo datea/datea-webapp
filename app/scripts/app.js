803b150c2b0d9902881015dd61dd75329368bce4
Juan La Jara <juanlajara@gmail.com>
2013-10-25 11:45:59 -0500

    CORS configuration and endpoint test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,11 @@
 'use strict';
 
-angular.module( 'dateaWebApp', [ ] )
-	.config( [ '$routeProvider', function ( $routeProvider ) {
+var datea = angular.module( 'dateaWebApp', [ 'ngResource' ] )
+	.config( [ '$routeProvider', '$httpProvider',
+	function ( $routeProvider, $httpProvider ) {
+
+		$httpProvider.defaults.useXDomain = true;
+		delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
 		$routeProvider
 		.when( '/' , { templateUrl : 'views/main.html'
@@ -10,3 +14,19 @@ angular.module( 'dateaWebApp', [ ] )
 		.otherwise( { redirectTo : '/' } );
 
 	} ] );
+
+// datea.factory( 'User', [ '$http', '$resource', '$log' , function ( $http, $resource, $log ) {
+// 	var url
+// 	  , post
+// 	  ;
+
+// 	url = 'http://192.168.1.37:8000/api/v2/create_user/';
+
+// 	save = { method: 'POST'
+// 	       , params: {}
+// 	       , format: 'json'
+// 	       }
+
+// 	return $resource( url, {}, post )
+
+// } ] )
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,27 @@
 'use strict';
 
 angular.module( 'dateaWebApp' )
-	.controller( 'MainCtrl', [ '$scope', function ( $scope ) {
+	.controller( 'MainCtrl', [ '$scope', '$http', function ( $scope, $http ) {
 
 		$scope.datea = {};
+		$scope.auth  = {};
+		// datea-api endpoint
+		$scope.auth.url   = 'http://192.168.1.37:8000/api/v2/create_user/';
 		$scope.datea.name = 'Datea.pe';
+
+		$scope.auth.save = function () {
+			var data;
+
+			data = { bio        : $scope.auth.bio
+			       , email      : $scope.auth.email
+			       , password   : $scope.auth.password
+			       , twitter_id : $scope.auth.twitterId }
+
+
+			$http.post( $scope.auth.url, data )
+			.success( function ( response ) {
+				console.log( response );
+			} );
+		}
 
 	} ] );
