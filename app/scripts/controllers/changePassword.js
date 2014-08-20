9357016984a18db5bca6707e4af898ce5976dbd3
Rodrigo Derteano <rodrigoderteano@gmail.com>
2014-08-20 19:22:39 -0300

    change password form beta 1

--- app/scripts/controllers/changePassword.js
+++ app/scripts/controllers/changePassword.js
@@ -18,8 +18,51 @@ angular.module( 'dateaWebApp' )
 ) {
 
 	$scope.flow     = {};
+	$scope.auth     = {};
 	$scope.alerts   = [];
 
-	console.log()
+	$scope.flow.loading                 = false;
+	$scope.flow.validInput              = {};
+	$scope.flow.validInput.password     = null;
+	$scope.flow.validInput.samePassword = null;
+
+	$scope.flow.changeSuccess           = null;
+
+	$scope.flow.checkPassword = function () {
+		if ( $scope.auth.password ) {
+			$scope.flow.validInput.password = /^(?=.*\d)(?=.*[a-z])(?!.*\s).{6,32}$/.test( $scope.auth.password );
+			$scope.auth.message = null;
+		}
+	}
+
+	$scope.flow.checkSamePassword = function () {
+		if ( $scope.auth.samePassword ) {
+			$scope.flow.validInput.samePassword = $scope.auth.samePassword === $scope.auth.password;
+			$scope.auth.message = null;
+		}
+	}
+
+	$scope.flow.savePasswd = function () {
+		var isValid;
+		isValid = $scope.form.$valid;
+
+		if (isValid) {
+			$scope.flow.loading = true;
+			var givens = {
+				  uid      : $routeParams.uid
+				, token    : $routeParams.token
+				, password : $scope.auth.password 
+			}
+
+			Api.account.changePassword(givens)
+			.then(function (response){
+				$scope.flow.changeSuccess = true;
+				$scope.flow.loading = false;
+			}, function (reason) {
+				$scope.flow.changeSuccess = false;
+				$scope.flow.loading = false;
+			});
+		}
+	}
 
 } ] );
