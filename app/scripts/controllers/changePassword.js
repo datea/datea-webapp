d3fd9558588296f61ea51930dbb5f6fc985905b2
Rodrigo Derteano <rodrigoderteano@gmail.com>
2014-08-20 16:30:57 -0300

    change pw wip

---
+++ app/scripts/controllers/changePassword.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module( 'dateaWebApp' )
+.controller( 'ChangePasswordCtrl'
+, [ '$scope'
+	, 'Api'
+	, 'User'
+	, '$location'
+	, 'config'
+	, '$routeParams'
+, function (
+		$scope
+	, Api
+	, User
+	, $location
+	, config
+	, $routeParams
+) {
+
+	$scope.flow     = {};
+	$scope.alerts   = [];
+
+	console.log()
+
+} ] );
